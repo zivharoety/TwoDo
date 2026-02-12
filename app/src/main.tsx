e22d82b3e3ad2a43cea4baa460e0775275c7d62d
ziv haroety <haroety@zivs-MacBook-Pro.local>
2026-02-12 11:00:26 +0200

    Cleanup and minor adjustments for deployment stability

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+console.log('Main.tsx starting...');
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <App />
